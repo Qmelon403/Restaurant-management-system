e2a8a3a70eb86d6d01a1cf96199e563c35fa21e8
Qmelon403 <qmelonr8@gmail.com>
2026-09-05 22:36:26 +0530

    made changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,10 @@
 import { useNavigate } from "react-router-dom"
 import Ferrofluid from "../assets/Ferrofluid"
+import { useState } from "react";
 
 function Login() {
     const navigate = useNavigate();
+    const [status, setstatus] = useState();
     const handlesubmit = (e) => {
         e.preventDefaults();
     };
@@ -33,7 +35,7 @@ function Login() {
 
             <section className="flex items-center justify-center min-h-screen relative z-5">
 
-                <div className="w-[40%] h-[50vh] shadow-xl rounded-[13px] text-white bg-[#c6cbf4]/20 backdrop-blur-[18px]" >
+                <div className="w-[40%] min-h-[50vh] relative shadow-xl rounded-[13px] text-white bg-[#c6cbf4]/20 backdrop-blur-[18px]" >
                     <form onSubmit={handlesubmit}>
                         <h1 className="text-center m-5 font-bold text-[30px]">Login</h1>
                         <div className="flex flex-col gap-2 m-4">
@@ -42,11 +44,19 @@ function Login() {
                             <label className="text-[20px]" htmlFor="">password</label>
                             <input className="w-full border-2 border-[#e6e619] h-[37px] rounded-[8px] px-4" type="password" placeholder="Password" name="password" required />
                         </div>
+                        <div className="text-center p-10 flex items-center   w-full  justify-center gap-8 ">
+                            <div onClick={() => setstatus("Employee")} className={`border-2 rounded-[15px] cursor-pointer w-40 p-6 ${status === "Employee" ? "border-[#add63c]" : "border-[#fafafa]"}`}>Employee</div>
+                            <div onClick={() => setstatus("Admin")} className={`border-2 rounded-[15px] cursor-pointer w-40 p-6 ${status === "Admin" ? "border-[#add63c]" : "border-[#fafafa]"}`}>Admin</div>
+
+                        </div>
+                        <div className="text-center w-full">
+                            {status === "Employee" && <p>Don't have an account <span className="cursor-pointer hover:text-[#1b33bf]">Register</span></p>}
+                        </div>
 
 
-                        <div className="flex justify-between absolute inset-x-0 bottom-0 m-10">
-                            <button className="p-4 rounded-[14px] text-[18px] bg-[#6a64ff] w-[30%]" type="button" onClick={() => navigate("/")}>Back</button>
-                            <button className="p-4 rounded-[14px] text-[18px] bg-[#6a64ff] w-[30%]" type="submit">Login</button>
+                        <div className="flex justify-between   inset-x-0 bottom-10 m-10">
+                            <button className="p-4 rounded-[14px] text-[18px] bg-[#6a64ff] w-[30%] transition-all duration-300 ease-in-out hover:bg-[#46ace8]" type="button" onClick={() => navigate("/")}>Back</button>
+                            <button className="p-4 rounded-[14px] text-[18px] bg-[#6a64ff] w-[30%] transition-all duration-300 ease-in-out hover:bg-[#46ace8]" type="submit">Login</button>
                         </div>
                     </form>
                 </div>
