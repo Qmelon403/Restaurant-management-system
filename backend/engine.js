6841d342836f50049dd25149afb981e7b0bb91a7
Qmelon403 <qmelonr8@gmail.com>
2026-09-19 12:27:47 +0530

    calculation for rngine

--- backend/engine.js
+++ backend/engine.js
@@ -13,3 +13,28 @@ function calculateproduct(ingredients, selling_price, dtaingred) {
 
 
 };
+
+function dataingred(ingredients, dtaingred) {
+
+    const results = ingredients.map((ingred) => {
+
+        const dtaresults = dtaingred.find(
+            (dta) => dta.ingredient_id === ingred.ingredient_id
+        );
+
+        return {
+            ingredient_id: ingred.ingredient_id,
+            name: ingred.name,
+            unit_cost: dtaresults.cost / 1000,
+            quantity: ingred.quantity,
+            totalcost: (dtaresults.cost * ingred.quantity) / 1000
+        };
+    });
+
+    const total = results.reduce(
+        (sum, ingr) => sum + ingr.totalcost,
+        0
+    );
+
+    return total;
+}
