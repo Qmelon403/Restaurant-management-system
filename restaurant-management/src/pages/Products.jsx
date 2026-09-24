d86b4f3df8c31c9783f0e5eda99c694b01c0cd06
Qmelon403 <qmelonr8@gmail.com>
2026-09-24 11:16:19 -0700

    product

--- restaurant-management/src/pages/Products.jsx
+++ restaurant-management/src/pages/Products.jsx
@@ -1,6 +1,36 @@
+import Ferrofluid from "../assets/Ferrofluid"
 function Products() {
     return (
-        <h2>Products</h2>
+        <>
+
+            <div className="relative min-h-screen isolate ">
+                {/* background layer, pinned behind everything */}
+                <div className="absolute inset-0 -z-10 bg-slate-950">
+                    <Ferrofluid
+                        colors={["#8c820d", "#5d38d7", "#457a96"]}
+                        speed={0.6}
+                        scale={1.8}
+                        turbulence={0.9}
+                        fluidity={0.11}
+                        rimWidth={0.22}
+                        sharpness={2.5}
+                        shimmer={1.5}
+                        glow={2.4}
+                        flowDirection="left"
+                        opacity={1}
+                        mouseInteraction
+                        mouseStrength={1}
+                        mouseRadius={0.35}
+                    />
+                </div>
+
+
+
+                <main className="relative z-5">
+                    <h2 className="text-white">Hello world</h2>
+                </main>
+            </div>
+        </>
     )
 }
 export default Products
